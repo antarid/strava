79912884838274f5d497d76bc13be8080b06b4d1
antarid <tarand007@gmail.com>
2019-05-05 20:51:52 +0300

    prettified

--- components/RunScreen/Controls.js
+++ components/RunScreen/Controls.js
@@ -1,40 +1,50 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import RoundButton from '../RoundButton';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 class Controls extends React.Component {
-  render() {
-    const startRunButton = (
-      <RoundButton key={0} name="play" onPress={this.props.startRun} />
-    );
-    const pauseRunButton = (
+  startButton = () => (
+    <RoundButton key={0} name="play" onPress={this.props.startRun} />
+  );
+
+  pauseRunButton = () => {
+    const { isPaused, unpauseRun, pauseRun } = this.props;
+    return (
       <RoundButton
         key={1}
-        name={this.props.isPaused ? 'play' : 'pause'}
-        onPress={
-          this.props.isPaused ? this.props.unpauseRun : this.props.pauseRun
-        }
+        name={isPaused ? 'play' : 'pause'}
+        onPress={isPaused ? unpauseRun : pauseRun}
       />
     );
-    const stopRunButton = (
-      <RoundButton key={2} name="square" onPress={this.props.stopRun} />
-    );
-    const toggleModeButton = (
-      <RoundButton
-        key={3}
-        name={this.props.mode === 'map' ? 'pulse' : 'map'}
-        onPress={this.props.toggleMode}
-      />
-    );
+  };
+
+  stopRunButton = () => (
+    <RoundButton key={2} name="square" onPress={this.props.stopRun} />
+  );
+
+  toggleModeButton = () => (
+    <RoundButton
+      key={3}
+      name={this.props.mode === 'map' ? 'pulse' : 'map'}
+      onPress={this.props.toggleMode}
+    />
+  );
+
+  render() {
+    const { isStarted, isPaused } = this.props;
+
     let content;
-
-    if (!this.props.isStarted) content = [startRunButton];
+    if (isStarted) content = [this.startRunButton()];
     else {
-      if (!this.props.isPaused) content = [pauseRunButton, toggleModeButton];
-      else content = [pauseRunButton, stopRunButton, toggleModeButton];
+      if (isPaused) content = [this.pauseRunButton(), this.toggleModeButton()];
+      else
+        content = [
+          this.pauseRunButton(),
+          this.stopRunButton(),
+          this.toggleModeButton()
+        ];
     }
-
     return <View style={styles.container}>{content}</View>;
   }
 }
@@ -55,9 +65,9 @@ export default connect(
     ...state.run
   }),
   dispatch => ({
-    startRun: () => dispatch({type: 'START_RUN'}),
-    pauseRun: () => dispatch({type: 'PAUSE_RUN'}),
-    unpauseRun: () => dispatch({type: 'UNPAUSE_RUN'}),
-    stopRun: () => dispatch({type: 'STOP_RUN'})
+    startRun: () => dispatch({ type: 'START_RUN' }),
+    pauseRun: () => dispatch({ type: 'PAUSE_RUN' }),
+    unpauseRun: () => dispatch({ type: 'UNPAUSE_RUN' }),
+    stopRun: () => dispatch({ type: 'STOP_RUN' })
   })
 )(Controls);
