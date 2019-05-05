84555ae1e31021314b75a3bcf0143d11a60ec05e
antarid <tarand007@gmail.com>
2019-05-05 23:13:32 +0300

    added redux-devtools-extension

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,4 +1,5 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import run from './reducers/run';
@@ -6,6 +7,9 @@ import run from './reducers/run';
 const rootReducer = combineReducers({
   run
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
